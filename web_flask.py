1cadfc624e1a8dc7633f5ac2c8e19f2129d1d0ae
kotosusl <108192472+kotosusl@users.noreply.github.com>
2023-03-01 00:46:50 +0700

    Add files via upload

--- web_flask.py
+++ web_flask.py
@@ -13,31 +13,42 @@ def index():
     return 'И на Марсе будут яблони цвести!'
 
 
-@app.route('/promotion')
-def promotion():
-    return '</br>'.join(['Человечество вырастает из детства.',
-                         'Человечеству мала одна планета.',
-                         'Мы сделаем обитаемыми безжизненные пока планеты.',
-                         'И начнем с Марса!',
-                         'Присоединяйся!'])
-
-
-@app.route('/image_mars')
-def image_mars():
+@app.route('/promotion_image')
+def promotion_image():
     return f"""<!DOCTYPE html>
                 <html lang="en">
                 <head>
                     <meta charset="UTF-8">
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" 
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
                     <title>Привет, Марс!</title>
                 </head>
                 <body>
-                <h1>Жди нас, Марс!</h1>
+                    <h1>Жди нас, Марс!</h1>
                     <figure>
                         <img src="{url_for('static', filename='img/mars.jpg')}" alt="здесь должна была быть картинка, но не нашлась">
                         <figcaption>Вот она какая, красная планета.</figcaption>
                     </figure>
+                    <div class="alert alert-dark" role="alert">
+                        Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                        Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        Присоединяйся!
+                    </div>
                 </body>
                 </html>"""
 
